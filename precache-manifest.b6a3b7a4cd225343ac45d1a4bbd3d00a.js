4cdc05796bede028570dc58e6a0c23450d87f107
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-05 11:49:14 +0000

    Deploy stvvt/react-cognito to github.com/stvvt/react-cognito.git:gh-pages

---
+++ precache-manifest.b6a3b7a4cd225343ac45d1a4bbd3d00a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8cf6ed7e3e12ad3f97e76fc28cac05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5e11b1bb85ab5c14c0e",
+    "url": "./static/js/2.182bdf52.chunk.js"
+  },
+  {
+    "revision": "4d1745f45a6e99543a6cd94a263b10c5",
+    "url": "./static/js/2.182bdf52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5921b8e7d375e0b7a0e1",
+    "url": "./static/js/main.e25402cf.chunk.js"
+  },
+  {
+    "revision": "a9ffbca5cad3e6ab7783",
+    "url": "./static/js/runtime-main.0b6082ab.js"
+  }
+]);
